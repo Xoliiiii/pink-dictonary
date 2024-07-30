ed2cd44237b21dd9a79ebfa5723855dbf49b8342
Xolisa Nunge <xolinunge0@gmail.com>
2024-07-30 23:17:25 +0200

    Creating skeleton for dictionary app

---
+++ src/components/Definition.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Definition() {
+  return (
+    <div>
+    <>
+      <h1 id="userWord">Word</h1>
+      <p id="pronunciation">
+        Lorem ipsum dolor 
+        </p>
+        <p id="userDefintion">
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        </p>
+      </>
+    </div>
+  )
+}
---
+++ src/components/Example.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Example() {
+  return (
+    <div>
+      <>
+      <h1>
+        Example
+      </h1>
+      <p id="exampleSentences">
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      </p>
+      </>
+    </div>
+  )
+}
